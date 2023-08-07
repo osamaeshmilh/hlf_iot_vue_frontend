a8fc9ef856e0364be399ac16bf5a393d7794122b
osamaeshmilh <osamaeshmilh@gmail.com>
2023-08-07 01:13:08 +0100

    i

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,60 @@
 <template>
-  <div id="app">
+  <div>
     <h1>Meds Data</h1>
-    <div v-for="item in medsData" :key="item.uniqueKey">
-      <p>{{ item.batchNo }} | {{ item.temperature }}°C | {{ item.humidity }}%</p>
-    </div>
+    <table>
+      <thead>
+      <tr>
+        <th>Batch No</th>
+        <th>Warehouse No</th>
+        <th>Temperature</th>
+        <th>Humidity</th>
+        <th>Timestamp</th>
+        <!-- Add other headers here -->
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="item in medsData" :key="item.uniqueKey">
+        <td>{{ item.batchNo }}</td>
+        <td>{{ item.warehouseNo }}</td>
+        <td>{{ item.temperature }}</td>
+        <td>{{ item.humidity }}</td>
+        <td>{{ item.timestamp }}</td>
+        <!-- Add other data columns here -->
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'App',
   data() {
     return {
       medsData: [],
     };
   },
   mounted() {
-    fetch('/api/getAllMedsData') // Adjust the URL to your actual endpoint
-        .then(response => response.json())
-        .then(data => {
-          this.medsData = data;
+    axios.get('http://localhost:3000/your-endpoint-path')
+        .then(response => {
+          this.medsData = response.data;
         })
-        .catch(error => console.error('There was an error fetching the data!', error));
+        .catch(error => {
+          console.error('There was an error!', error);
+        });
   },
 };
 </script>
 
 <style>
-/* Add any styles you need here */
+/* Add some basic styling to the table */
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 10px;
+  border: 1px solid #ccc;
+}
 </style>
